acc52626c664fad74cf39288f7028b292396f9b2
Massimo Brigola <c24massimo.brigola@afacademy.af.edu>
2023-10-23 10:13:14 -0600

    updated to be used for multiple trials

--- speed_test_client.py
+++ speed_test_client.py
@@ -2,35 +2,40 @@ import socket
 import time
 from random import randbytes
 
+DATA_SIZE = 1000000
+
 # IP address and port of the server
 server_ip = 'DF-2TK21604MF'  
 server_port = 12345 
 #server_ip = socket.gethostname()
 
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-client_socket.connect((server_ip, server_port))
-
+outputFile = open("client_results.csv", "a")
+outputFile.write("Trial #,Data Transfer Time (ns),Data Size (Bytes),Data Transfer Rate (KB/s)")
 # Define the data to send
-data_to_send = randbytes(1000000)
+data_to_send = randbytes(DATA_SIZE)
 
-try:
+for i in range(50):
+    client_socket.connect((server_ip, server_port))
+        
     start_time = time.time_ns()  # Record the start time
 
     # Send the data to the server
     client_socket.send(data_to_send)
-    #print(data_to_send)
 
     end_time = time.time_ns()  # Record the end time
 
     # Calculate the transfer time
     transfer_time = end_time - start_time
-    print(f"Data transfer time: {transfer_time:.4f} seconds")
 
     # Calculate and print the data transfer rate
     data_rate = len(data_to_send) / transfer_time*pow(10,9) / 1024  # In KB/s
-    print(f"Data transfer rate: {data_rate:.2f} KB/s")
-
-finally:
+    outputFile.write(f"{i+1},{transfer_time},{DATA_SIZE},{data_rate}\n")
+    
+    print(f"Trial {i}: {data_rate} KB/s")
     client_socket.close()
+    
+outputFile.close()
--- speed_test_server.py
+++ speed_test_server.py
@@ -3,37 +3,42 @@ import time
 
 # server IP and port
 #server_ip = '192.168.1.2'
+
+DATA_SIZE = 1000000
+
 server_port = 12345
 server_ip = socket.gethostname()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind((server_ip, server_port))
+outputFile = open("server_results.csv", "a")
+outputFile.write("Trial #,Data Transfer Time (ns),Data Size (Bytes),Data Transfer Rate (KB/s)")
 
-server_socket.listen(5)
 
-print(f"\nServer is listening on {server_ip}:{server_port}")
+for i in range(50):
+    server_socket.bind((server_ip, server_port))
 
-while True:
-    # Accept a connection from a client
+    server_socket.listen()
     client_socket, client_address = server_socket.accept()
-    print(f"Accepted connection from {client_address}")
-
+    
+    print(f"Accepted connection for trial #{i+1}")
+    
     start_time = time.time_ns()  # Record the start time
 
     # Receive data from the client
-    received_data = client_socket.recv(10000000)
-    #print(received_data)
+    received_data = client_socket.recv(DATA_SIZE)
 
     end_time = time.time_ns()  # Record the end time
 
     # Calculate the transfer time
     transfer_time = end_time - start_time
-    print(f"Data transfer time: {transfer_time:.4f} nanoseconds")
-
+    
     # Calculate and print the data transfer rate
     data_rate = float(len(received_data)) / transfer_time*pow(10,9) /1024 # In KB/s
-    print(f"Data transfer rate: {data_rate:.2f} KB/s")
-
+    
+    outputFile.write(f"{i+1},{transfer_time},{DATA_SIZE},{data_rate}\n")
+    
     # Close the client socket
-    client_socket.close()
+    server_socket.close()
+
+outputFile.close()
